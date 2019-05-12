fee914598f3146b1ed9c3fbe57867926d7314afa
Huy Hoang <nhox.no.one@gmail.com>
2019-05-12 18:29:37 +0700

    End of section 11

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 import {JwtHelperService} from '@auth0/angular-jwt';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +16,13 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     const helper = new JwtHelperService();
     const token = localStorage.getItem('token');
+    const user: User  = JSON.parse(localStorage.getItem('user'));
     if (token) {
       this.autheService.decodedToken =  helper.decodeToken(token);
+    }
+    if(user) {
+      this.autheService.currentUser = user;
+      this.autheService.changeMemberPhoto(user.photoUrl);
     }
   }
 }
